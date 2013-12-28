4a7a8615a058465525b33358e29990fca12e0101
Colin Mackey <colin@fishbrains.com>
2013-12-28 13:00:52 -0500

    app framework

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+var express = require('express');
+var app = express();
+
+var tickers = [
+	{ symbol : 'MSFT', name : 'Microsoft Corporation'},
+	{ symbol : 'AAPL', name : 'Apple Inc.'},
+	{ symbol : 'GOOG', name : 'Google Inc.'},
+	{ symbol : 'AMZN', name : 'Amazon.com Inc.'}
+];
+
+app.use(express.bodyParser());
+
+// /stock
+
+// /stock/[symbol]
+
+// /stock/random
