9520f2aed7d62366ca605d49bbe17c61a1b6d3b0
Colin Mackey <colin@fishbrains.com>
2013-12-30 12:41:35 -0500

    added stock update

--- server.js
+++ server.js
@@ -8,19 +8,9 @@ var models = require('./models');
 var routes = require('./routes');
 var middleware = require('./middleware');
 
-var schema = mongoose.Schema({
-    symbol: {
-        type: String,
-    },
-    name: {
-        type: String
-    },
-    price: {
-        type: Number
-    }
-});
-
 mongoose.set('debug', true);
+
+mongoose.connect('mongodb://localhost/tickers');
 
 app.configure(function () {
     app.use(express.bodyParser());
@@ -28,43 +18,83 @@ app.configure(function () {
     app.use(app.router);
 });
 
+var schema = mongoose.Schema;
 
-// /stock: lists all stock prices (from array)
-app.get('/stock', function (req, res) {
-    res.json(tickers);
+var stock = new Schema({
+    symbol: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    modified: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-// add a stock to the array
+var stockModel = mongoose.model('stock', stock);
+
+// /stock: lists all stock prices (from database)
+app.get('/stock', function (req, res) {
+    return stockModel.find(function (err, stocks) {
+        if (!err)
+            return res.send(stocks);
+        else
+            return console.log(err);
+    });
+});
+
+// add a stock to the database
 app.post('/stock', function (req, res) {
-    if (!req.body.hasOwnProperty('symbol') || !req.body.hasOwnProperty('name') || !req.body.hasOwnProperty('price')) {
-        res.statusCode = 400;
-        return res.send('Error 400: Post syntax incorrect.');
-    }
-    var s = req.body.symbol.toUpperCase();
-    var newStock = {
-        symbol: s,
+    var stock;
+    console.log("POST: ");
+    console.log(req.body);
+    product = new stockModel({
+        symbol: req.body.symbol.toUpperCase(),
         name: req.body.name,
         price: req.body.price
-    };
-
-    for (var i = 0; i < tickers.length; i++) {
-        if (tickers[i].symbol === s) {
-            res.statusCode = 409;
-            return res.send('Error 409: Entry already exists');
-        }
-    }
-    tickers.push(newStock);
-    res.send('New stock added!');
+    });
+    stock.save(function (err) {
+        if (!err)
+            return console.log("Created");
+        else
+            return console.log(err);
+    });
+    return res.send(stock);
 });
 
-// /stock/random: lists price from a random stock (from array)
+// /stock/random: lists price from a random stock (from database)
 app.get('/stock/random/', function (req, res) {
     var id = Math.floor(Math.random() * tickers.length);
     var s = tickers[id];
     res.json(s);
 });
 
-// /stock/[symbol]: gets current price for [symbol] (from array)
+// UPDATE a stock by ID
+app.put('/stock/:s', function (req, res) {
+    return stockModel.findById(req.params.s, function(err, stock) {
+        stock.symbol = req.body.symbol.toUpperCase();
+        stock.name = req.body.name;
+        stock.price = req.body.price;
+        return stock.save(function(err) {
+            if(!err)
+                console.log("updated");
+            else
+                console.log(err);
+            return res.send(stock);
+        });
+    });
+});
+
+// /stock/[symbol]: gets current price for [symbol] (from database)
 app.get('/stock/:s', function (req, res) {
     var s = req.params.s.toUpperCase();
     for (var i = 0; i < tickers.length; i++) {
@@ -76,7 +106,7 @@ app.get('/stock/:s', function (req, res) {
     return res.send('Error 404: Symbol not found');
 });
 
-// delete a stock from the array
+// delete a stock from the database
 app.delete('stock/:s', function (req, res) {
     var s = req.params.s.toUpperCase();
 
